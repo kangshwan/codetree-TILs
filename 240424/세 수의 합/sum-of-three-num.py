be5c5cb9870dc2edd35976b8ffc333fbc70eb26c
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-04-24 08:21:47 +0900

    Title: 세 수의 합, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240424/세 수의 합/sum-of-three-num.py
+++ 240424/세 수의 합/sum-of-three-num.py
@@ -1,10 +1,30 @@
 N, K = map(int, input().split())
 nums = list(map(int, input().split()))
 
-answer = 0
+# 각 숫자가 몇 번 나왔는지 기록
+# in hash map
+
+count = {}
+for num in nums:
+    if num in count:
+        count[num] += 1
+    else:
+        count[num] = 1
+
+ans = 0
+# i가 고정되어있을 때, i 보다 작은 j와 i 보다 큰 k의 값을 hashmap으로 구현한다.
+
+# 배열을 앞에서 부터 순회한다.
 for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
-            if nums[i]+nums[j]+nums[k] == K:
-                answer += 1
-print(answer)
+    # i는 이미 순회했다고 가정하기 때문에, count에서 i번째를 제거한다.
+    count[nums[i]] -= 1
+    
+    for j in range(i):
+        # j의 경우 i보다 작고, 이미 순회했다고 가정하며 count에서 제거되었을 것이다.
+        # 그렇다면, k에서 nums[i]와 nums[j]를 뺀 값인 diff가 count에 존재한다면, 
+        # count[diff]개의 diff가 i 보다 큰 k 인덱스들에 있다는 의미가 된다!
+        diff = K - nums[i] - nums[j]
+        if diff in count:
+            ans += count[diff]
+
+print (ans)
