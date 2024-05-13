1e79b997735f098ea20cae1d87e538bb5df5fcaf
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-05-13 16:18:04 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240513/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int arr[100];
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0 ; i < n-1 ; i++){
+        for(int j = 0 ; j < n-1 ; j++){
+            if(arr[j] > arr[j+1]){
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+    for(int i = 0 ; i < n ; i++){
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
