2c47b0757c7e187450c71062f059f8bbe8a3de9d
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-06-05 23:48:49 +0900

    Title: 거꾸로 순열, Time: 40ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240605/거꾸로 순열/backward-permutation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#define MAX_N 8
+
+using namespace std;
+
+vector<int> permu;
+bool visited[MAX_N+1];
+int N;
+void Permutation(int cnt){
+    if(cnt == N){
+        for(auto k: permu){
+            cout << k << ' ';
+        }cout << '\n';
+        return;
+    }
+    for(int i = N ; i >= 1 ; i--){
+        if(!visited[i]){
+            visited[i] = true;
+            permu.push_back(i);
+            Permutation(cnt+1);
+            permu.pop_back();
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> N;
+    Permutation(0);
+    return 0;
+}
