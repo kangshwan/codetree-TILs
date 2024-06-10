46f78f75316a4900e0d42291567e91f032be71e3
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-06-10 23:29:28 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240610/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define MAX_N 19
+
+using namespace std;
+
+int N;
+int DP[MAX_N+1] = {0, 1, 2, 5};
+int main() {
+    cin >> N;
+    for(int i = 4 ; i <= N ; i++){
+        int tmp = 0;
+        for(int j = 0 ; j <= i ; j++){
+            tmp += DP[i-j] * DP[j];
+        }
+        DP[i] = tmp;
+    }
+    cout << DP[N];
+    return 0;
+}
+
+/*
+뭔가 leaf 개수를 저장하고 있는 게 필요해보인다.
+
+*/
