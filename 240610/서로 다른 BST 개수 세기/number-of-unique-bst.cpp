28a4fc7a71f910cf8bd2d9c85f3979f307a120de
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-06-10 23:30:56 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240610/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
+++ 240610/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int N;
-int DP[MAX_N+1] = {0, 1, 2, 5};
+int DP[MAX_N+1] = {1, 1, 2, 5};
 int main() {
     cin >> N;
     for(int i = 4 ; i <= N ; i++){
         int tmp = 0;
         for(int j = 0 ; j <= i ; j++){
-            tmp += DP[i-j] * DP[j];
+            tmp += DP[i-j] * DP[j-1];
         }
         DP[i] = tmp;
     }
