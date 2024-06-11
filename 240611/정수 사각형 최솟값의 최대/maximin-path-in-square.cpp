cf41bded3fb0e25f4654875c2e4b4af27d692e31
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-06-11 20:02:42 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240611/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240611/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -23,7 +23,7 @@ int main() {
     }
     for(int i = 1 ; i < N ; i++){
         for(int j = 1 ; j < N ; j++){
-            DP[i][j] = min(max(DP[i-1][j], DP[i][j-1]), board[i][j])
+            DP[i][j] = min(max(DP[i-1][j], DP[i][j-1]), board[i][j]);
         }
     }
     cout << DP[N-1][N-1];
