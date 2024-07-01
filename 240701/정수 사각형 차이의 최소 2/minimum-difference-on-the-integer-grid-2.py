7d20fe77e08ea2f4a1b0a16c220183ede0fc67f9
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-01 20:00:35 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 452ms, Memory: 30MB, Status: Passed - CodeTree

--- 240701/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240701/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -9,10 +9,10 @@ for _ in range(N):
 def check():
     dp = [[10e9 for _ in range(N)] for _ in range(N)]
     dp[0][0] = board[0][0]
-
-    for i in range(1,N):
+    for i in range(1, N):
         dp[i][0] = max(dp[i-1][0], board[i][0])
         dp[0][i] = max(dp[0][i-1], board[0][i])
+    for i in range(1,N):
         for j in range(1, N):
             dp[i][j] = max(min(dp[i-1][j], dp[i][j-1]), board[i][j])
     return dp[N-1][N-1]
