2dad9b7100450abe2fdff2ce22e38e4d6bf32eec
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-01 11:48:45 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240701/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240701/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -25,19 +25,28 @@ for i in range(1,N):
     for j in range(1, N):
         upper = find_max_min(dp[i-1][j], board[i][j])
         left = find_max_min(dp[i][j-1], board[i][j])
-        # 최대값이 작을수록 유리
-        if upper[0] < left[0]:
+        if upper[1] > left[1]:
             dp[i][j] = upper
-        elif left[0] < upper[0]:
+        elif upper[1] < left[1]:
             dp[i][j] = left
         else:
-            # 최대값이 동일하다면, 최소값이 클수록 유리
-            if upper[1] > left[1]:
+            if upper[0] < left[0]:
                 dp[i][j] = upper
             else:
                 dp[i][j] = left
+        # # 최대값이 작을수록 유리
+        # if upper[0] < left[0]:
+        #     dp[i][j] = upper
+        # elif left[0] < upper[0]:
+        #     dp[i][j] = left
+        # else:
+        #     # 최대값이 동일하다면, 최소값이 클수록 유리
+        #     if upper[1] > left[1]:
+        #         dp[i][j] = upper
+        #     else:
+        #         dp[i][j] = left
 
 def calc_diff(data):
     return data[0] - data[1]
-    
+
 print(calc_diff(dp[N-1][N-1]))
