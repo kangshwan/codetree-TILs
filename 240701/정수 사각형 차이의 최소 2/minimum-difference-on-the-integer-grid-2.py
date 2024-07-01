41ba49d9189a0bb6e04a6190d4e2aa197146e49b
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-01 19:59:48 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240701/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240701/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -2,56 +2,34 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 board = []
-dp = [[() for _ in range(N)] for _ in range(N)]
 
 for _ in range(N):
     board.append(list(map(int, input().split())))
 
-lowerbound = 3
+def check():
+    dp = [[10e9 for _ in range(N)] for _ in range(N)]
+    dp[0][0] = board[0][0]
 
-def find_max_min(before, num):
-    global lowerbound
-    max_n, min_n = before
-    if min_n <= num <= max_n:
-        if min_n > lowerbound:
-            lowerbound = min_n
-        return before
-    elif num < min_n:
-        if num > lowerbound:
-            lowerbound = num
-        return (max_n, num)
-    else:
-        if min_n < lowerbound:
-            lowerbound = min_n
-        return (num, min_n)
+    for i in range(1,N):
+        dp[i][0] = max(dp[i-1][0], board[i][0])
+        dp[0][i] = max(dp[0][i-1], board[0][i])
+        for j in range(1, N):
+            dp[i][j] = max(min(dp[i-1][j], dp[i][j-1]), board[i][j])
+    return dp[N-1][N-1]
 
-dp[0][0] = (board[0][0], board[0][0])
-for i in range(1, N):
-    dp[0][i] = find_max_min(dp[0][i-1], board[0][i])
-    dp[i][0] = find_max_min(dp[i-1][0], board[i][0])
+# 숫자가 1~100이기 때문에, 최소값을 고정해가며 경로를 찾는다.
 
-for i in range(1,N):
-    for j in range(1, N):
-        board_num = board[i][j]
-        upper = find_max_min(dp[i-1][j], board_num)
-        left = find_max_min(dp[i][j-1], board_num)
-        # 최대값이 작을수록 유리
-        if upper[0] < left[0]:
-            dp[i][j] = upper
-            lowerbound = upper[1]
-        elif left[0] < upper[0]:
-            dp[i][j] = left
-            lowerbound = left[1]
-        else:
-            # 최대값이 동일하다면, 최소값이 클수록 유리
-            if upper[1] > left[1]:
-                dp[i][j] = upper
-                lowerbound = upper[1]
-            else:
-                dp[i][j] = left
-                lowerbound = left[1]
+INF = 10e9
+ans = 10e9
+for low in range(1, 101):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] < low:
+                board[i][j] = INF
 
-def calc_diff(data):
-    return data[0] - data[1]
+    max_val = check()
+    if max_val == INF:
+        continue
+    ans = min(ans, max_val - low)
 
-print(calc_diff(dp[N-1][N-1]))
+print(ans)
