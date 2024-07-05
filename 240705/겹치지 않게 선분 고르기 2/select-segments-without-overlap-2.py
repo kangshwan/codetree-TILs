f4d53a61f041ccf11b0dd3d5b105097636bea345
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-05 23:10:32 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240705/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240705/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -7,6 +7,7 @@ for _ in range(N):
     lines.append(tuple(map(int, input().split())))
 lines.sort(key=lambda x: x[1])
 lines.sort()
+
 # dp[i]는 i번째 막대를 선택 or 선택하지 않는 경우 가장 많이 고른 개수
 # 정렬을 이미 한번 진행했기 때문에, 중복됐다면 이전 막대들을 확인하여 최대를 찾는다.
 dp = [0 for _ in range(N)]
@@ -14,15 +15,17 @@ dp[0] = 1
 def dup(line1, line2):
     s1,e1 = line1
     s2,e2 = line2
-    if s1 < s2 and e1 < e2:
-        return False
-    elif s1 > s2 and e1 > e2:
-        return False
-    return True
+    if e2 < e1:
+        return True
+    if s1 <= s2 <= e1:
+        return True
+    return False
+
 for idx in range(1, N):
     for prev in range(idx):
         if not dup(lines[prev], lines[idx]):
             dp[idx] = max(dp[idx], dp[prev] + 1)
     if dp[idx] == 0:
         dp[idx] = 1
+    
 print(max(dp))
