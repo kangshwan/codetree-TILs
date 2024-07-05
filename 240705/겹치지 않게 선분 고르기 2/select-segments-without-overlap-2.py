fbd8aa95c682dd3b5b1de539544f0c27458cda60
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-05 22:59:55 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240705/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240705/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -5,7 +5,7 @@ N = int(input())
 lines = []
 for _ in range(N):
     lines.append(tuple(map(int, input().split())))
-lines.sort(key=lambda x: x[1])
+# lines.sort(key=lambda x: x[1])
 lines.sort()
 # dp[i]는 i번째 막대를 선택 or 선택하지 않는 경우 가장 많이 고른 개수
 # 정렬을 이미 한번 진행했기 때문에, 중복됐다면 이전 막대들을 확인하여 최대를 찾는다.
