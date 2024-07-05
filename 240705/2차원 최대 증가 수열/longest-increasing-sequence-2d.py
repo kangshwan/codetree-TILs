98121d4c7af22f96d4162d9e6b02eef54282491d
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-05 11:15:42 +0900

    Title: 2차원 최대 증가 수열, Time: 145ms, Memory: 26MB, Status: Passed - CodeTree

--- 240705/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240705/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -10,11 +10,14 @@ dp = [[-1 for _ in range(M)]for _ in range(N)]
 dp[0][0] = 1
 for x in range(N):
     for y in range(M):
+
         if dp[x][y] != -1:
+            # 점프뛸 수 있는 위치인지 확인
             for nx in range(x+1, N):
-                for ny in range(y+1, N):
+                for ny in range(y+1, M):
+                    # 이전 위치보다 값이 큰지 확인
                     if board[nx][ny] > board[x][y]:
-                        dp[nx][ny] = dp[x][y] + 1
+                        dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
 ans = -1
 for i in dp:
     ans = max(ans, max(i))
