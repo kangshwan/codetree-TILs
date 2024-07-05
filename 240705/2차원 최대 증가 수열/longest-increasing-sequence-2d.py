5c93b1dda675bed7475995188c3ae430cd50a1c8
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-05 11:09:13 +0900

    Title: 2차원 최대 증가 수열, Time: 182ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240705/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = []
+for _ in range(N):
+    board.append(list(map(int, input().split())))
+
+dp = [[-1 for _ in range(M)]for _ in range(N)]
+dp[0][0] = 1
+for x in range(N):
+    for y in range(M):
+        if dp[x][y] != -1:
+            for nx in range(x+1, N):
+                for ny in range(y+1, N):
+                    if board[nx][ny] > board[x][y]:
+                        dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
+ans = -1
+for i in dp:
+    ans = max(ans, max(i))
+print(ans)
