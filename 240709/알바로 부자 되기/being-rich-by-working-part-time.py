8870ef3dca87d635c55ab8fc0fb1865ff2394070
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-09 11:50:46 +0900

    Title: 알바로 부자 되기, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240709/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240709/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -16,29 +16,4 @@ for i in range(1, len(infos)):
         if t_e < s:
             max_able = max(max_able, dp[j])
     dp[i] = p + max_able
-# print(dp)
 print(max(dp))
-
-# import sys
-# input = sys.stdin.readline
-
-# N = int(input())
-# infos = []
-# for _ in range(N):
-#     s, e, p = map(int, input().split())
-#     infos.append((s,e,p))
-# dp = [[0 for _ in range(len(infos)] for _ in range(2)]
-# dp[1][0] = infos[0][2]
-# for i in range(1, len(infos)):
-#     # i번째 일을 하지 않는 경우
-#     dp[0][i] = max(dp[0][i-1], dp[1][i-1])
-#     # i번째 일을 하는 경우
-#     s, e, p = infos[i]
-#     max_able = 0
-#     for j in range(i):
-#         t_s, t_e, _ = infos[j]
-#         if t_e < s:
-#             max_able = max(max_able, dp[j])
-#     dp[i] = max(dp[i-1], p + max_able)
-# print(dp)
-# # print(max(dp))
