1f7426b80eb91added178b7f0db5ba3293e68b5b
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-07-09 11:29:55 +0900

    Title: 알바로 부자 되기, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240709/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240709/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -3,21 +3,18 @@ input = sys.stdin.readline
 
 N = int(input())
 infos = []
-max_day = 0
 for _ in range(N):
     s, e, p = map(int, input().split())
     infos.append((s,e,p))
-    max_day = max(max_day, e)
-
-dp = [0] * (max_day + 1)
-idx = 0
-for i in range(max_day + 1):
-    dp[i] = dp[i-1]
-    while idx < len(infos):
-        s, e, p = infos[idx]
-        if e != i:
-            break
-        dp[e] = max(dp[e], p + dp[s-1])
-        idx += 1
-
-print(dp[max_day])
+dp = [0] * len(infos)
+dp[0] = infos[0][2]
+for i in range(1, len(infos)):
+    s, e, p = infos[i]
+    max_able = 0
+    for j in range(i):
+        t_s, t_e, _ = infos[j]
+        if t_e < s:
+            max_able = max(max_able, dp[j])
+    dp[i] = max(dp[i-1], p + max_able)
+# print(dp)
+print(max(dp))
