17e39769f848cc452950fba0a77f493ec817ca99
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-08-26 10:58:16 +0900

    Title: 최대 동전 거슬러주기, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 240826/최대 동전 거슬러주기/max-coin-change.py
+++ 240826/최대 동전 거슬러주기/max-coin-change.py
@@ -6,10 +6,14 @@ DP = [-1 for _ in range(10001)]
 for coin in coins:
     DP[coin] = 1
 for price in range(1, M+1):
-    # DP[i] = max(DP[i-c_1]+DP[c_1], DP[i-c_2]+DP[c_2], ... , DP[i-c_N]) + 1
     tmp = DP[price]
     for coin in coins:
+        # price - coin >= 0 --> 현재 coin을 사용하여 price를 만들 수 있는지 확인
+        # DP[price-coin] != -1 --> price가 price - coin일 때, 동전들을 사용하여 만들 수 있는 경우가 있는지 확인.
+        # DP[price-coin] == -1이라는 뜻은, price가 price - coin일 때 동전들로 만들 수 없다는 뜻. 즉 현재 coin을 사용하여 price를 만들 수 없다!
         if price - coin >= 0 and DP[price-coin] != -1:
+            # DP[coin]은 일정하게 1이다.
+            # DP[price - coin]으로 만들 수 있기 때문에, + 1 을 더하고, 기존 값과 비교하여 더 큰지 확인한다.
             tmp = max(tmp, DP[coin]+DP[price-coin])
     DP[price] = tmp
 print(DP[M])
