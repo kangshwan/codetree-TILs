7295796d1c271fd5280427eebb69b1a6663eafe3
SeunghwanKang <46666296+kangshwan@users.noreply.github.com>
2024-08-26 10:44:22 +0900

    Title: 최대 동전 거슬러주기, Time: 113ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240826/최대 동전 거슬러주기/max-coin-change.py
+++ 240826/최대 동전 거슬러주기/max-coin-change.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
 coins = list(map(int, input().split()))
-DP = [0 for _ in range(10001)]
+DP = [-1 for _ in range(10001)]
 for coin in coins:
     DP[coin] = 1
-for price in range(max(coins), M+1):
-    # DP[i] = max(DP[i-c_1]+DP[c_1], DP[i-c_2]+DP[c_2], ... , DP[i-c_N]) + 1
-    tmp = 0
+for price in range(max(coins)+1, M+1):
+    tmp = -1
     for coin in coins:
-        if price - coin > 0:
+        if price - coin > 0 and DP[price-coin] != -1:
             tmp = max(tmp, DP[coin]+DP[price-coin])
     DP[price] = tmp
 # print(DP[:M+1])
